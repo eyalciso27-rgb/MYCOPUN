ca773249812b5a2aaed328dce8352be471f57bee
eyalciso27-rgb <eyalciso27@gmail.com>
2026-03-22 09:16:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+#
 const supabaseUrl = 'https://iaihwnbbebjsirlhezwb.supabase.co';
 const supabaseKey = 'sb_publishable_JOxGwNl2y2S-wUL2Xp3LLA_Xn9UxEHJ';
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
